29a4ce2b5903fb9a603706a19ef3fb65a695303e
Abhijeet Singh <143501136+abhi-singh-01@users.noreply.github.com>
2024-09-11 01:01:27 +0530

    Add files via upload

---
+++ TypeCast.java
@@ -0,0 +1,8 @@
+public class TypeCast {
+    public static void main(String[] args) {
+        float marks = 99.99f;
+        // int marks = marks;
+        int marks1 = (int) 99.99f;
+        System.out.println(marks1);
+    }
+}
